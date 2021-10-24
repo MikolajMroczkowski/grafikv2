8c46d2770fad4ab153491fd82ea67ed9ae2830ba
Mikołaj Mroczkowski <mikolaj.mroczkowski@gmail.com>
2021-10-25 00:39:04 +0200

    Poprawiono umieranie
    Zmieniono forme tekstu umierania mysqi

--- admAddeLock.php
+++ admAddeLock.php
@@ -12,7 +12,7 @@ if ($_GET) {
     require_once "config.php";
     $conn = new mysqli($dbserver, $dbusername, $dbpassword, $dbname);
     if ($conn->connect_error) {
-        die('<script> showalert("Błąd bazy","' . $conn->connect_error . '","alert-danger" </script>');
+        die('Błąd bazy:' . $conn->connect_error);
     }
     $conn->query("INSERT into blokada (year,date,month) values (".$_GET['year'].",'".$_GET['date']."',".$_GET['month'].")");
     $conn->close();
--- admRemoveLock.php
+++ admRemoveLock.php
@@ -12,7 +12,7 @@ if ($_GET) {
     require_once "config.php";
     $conn = new mysqli($dbserver, $dbusername, $dbpassword, $dbname);
     if ($conn->connect_error) {
-        die('<script> showalert("Błąd bazy","' . $conn->connect_error . '","alert-danger" </script>');
+        die('Błąd bazy:' . $conn->connect_error);
     }
     $conn->query("DELETE from blokada WHERE id=" . $_GET['id']);
     echo 'Usunięto';
